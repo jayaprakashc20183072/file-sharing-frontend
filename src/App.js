7448d5965e5e038e533e8e370f56f5b3fe24fed8
Jayaprakash Reddy <chembeti.jayaprakash-reddy@capgemini.com>
2023-05-29 16:55:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { uploadFile } from "./Services/Api";
+import { useRef, useState, useEffect } from "react";
 function App() {
+  const fileInputRef = useRef();
+  const [file, setFile] = useState("");
+  const [result, setResult] = useState("");
+  const openFiles = () => {
+    fileInputRef.current.click();
+  };
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+        // let response =
+        let response = await uploadFile(data);
+        setResult(response.path);
+      }
+    };
+    getImage();
+  }, [file]);
+  console.log(file);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrapper">
+        <h1>Simple File Sharing</h1>
+        <h2>Upload and share the download link</h2>
+        <div>
+          <button onClick={openFiles}>Upload</button>
+          <input
+            ref={fileInputRef}
+            type="file"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <a href={result} target="_blank">
+            <button className="downloadButton">Download</button>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
